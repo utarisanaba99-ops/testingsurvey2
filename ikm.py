1dc6033959962c1b7b05607be74a8df1d84f3c70
utarisanaba99-ops <utari.sanaba99@gmail.com>
2025-12-03 10:36:43 +0700

    Update ikm.py

--- ikm.py
+++ ikm.py
@@ -43,7 +43,7 @@ if st.button("Show"):
         if not student_courses.empty:
             # Check if the courses are filled in response data
             student_courses['IKM Sudah Terisi'] = student_courses['Matakuliah'].apply(
-                lambda x: 'Sudah' if x in response_data_filtered['Nama mata kuliah yang diampu sesuai nama dosen yang dipilih sebelumnya'].values else 'Belum'
+                lambda x: 'Sudah' if x in response_data_filtered['Mata Kuliah'].values else 'Belum'
             )
 
             # Apply color formatting for the 'IKM Sudah Terisi' column
